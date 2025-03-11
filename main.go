891f18ac82fe273dce8fbdc611543870ff75e683
Douglas Soule <souleman13@gmail.com>
2025-03-10 23:27:50 -0600

    break apart address cell

--- main.go
+++ main.go
@@ -2,9 +2,25 @@ package main
 
 import "strings"
 
-type AddressLineItem struct {
+type Customer struct {
 	Email string
-	Item string
+	FirstName string
+	LastName string
+	Phone string
+	Note string
+	Tags string
+}
+type Address struct {
+	AddressFirstName string
+	AddressLastName string
+	AddressCompany string
+	AddressPhone string
+	AddressLine1 string
+	AddressLine2 string
+	AddressCity string
+	AddressProvinceCode string
+	AddressCountry string
+	AddressZip string
 }
 
 
@@ -42,21 +58,67 @@ func main() {
 
 	//for addresses
 	for _, record := range records {
-		addresses := strings.Split(record[1], "|")
+		customer := Customer {
+			Email : record[4],
+			FirstName : record[1],
+			LastName : record[2],
+			Phone : record[5],
+			Note : "Joined Big Commerce: "+record[9]+" - "+record[6],
+			Tags : "BigCommerce,",
+		}
+		addresses := strings.Split(record[10], "|")
 		if len(addresses) > 1 {
 			for _, address := range addresses {
-				newAddressLine := AddressLineItem{
-					Email: record[0],
-					Item: address,
+				//Address First Name: Daniel, Address Last Name: Ramirez, Address Company: , 
+				// Address Line 1: 221 E. Indianola Ave, Address Line 2: , City/Suburb: Phoenix, 
+				// State Abbreviation: AZ, Zip/Postcode: 85012, Country: United States, Address Phone: 
+				newAddress := Address{}
+				addressParts := strings.Split(address, ",")
+				for _, addressValue := range addressParts {
+					values := strings.Split(addressValue, ":")
+					//bc col order
+					newAddress.AddressFirstName = values[0]
+					newAddress.AddressLastName = values[1]
+					newAddress.AddressCompany = values[2]
+					newAddress.AddressLine1 = values[3]
+					newAddress.AddressLine2 = values[4]
+					newAddress.AddressCity = values[5]
+					newAddress.AddressProvinceCode = values[6]
+					newAddress.AddressZip = values[7]
+					newAddress.AddressCountry = values[8]
+					newAddress.AddressPhone = values[9]
 				}
-				writeCSVRecord(writer, []string{newAddressLine.Email,newAddressLine.Item})
+				
+				writeCSVRecord(writer, []string{
+					//basic cols - email, command, first, last, phone, note, tags, tags command
+					customer.Email, "MERGE", customer.FirstName, customer.LastName, customer.Phone,customer.Note,customer.Tags, "MERGE",
+					//address cols - command, first, last, company, phone, line1, line2, city, provinceCode, country, zip
+					"MERGE", newAddress.AddressFirstName, newAddress.AddressLastName, newAddress.AddressCompany, newAddress.AddressPhone,
+					newAddress.AddressLine1, newAddress.AddressLine2, newAddress.AddressCity, newAddress.AddressProvinceCode, 
+					newAddress.AddressCountry, newAddress.AddressZip,
+				})
 			}
 		} else {
-			newAddressLine := AddressLineItem{
-				Email: record[0],
-				Item: record[1],
+			addressVals := Address{
+				AddressFirstName : "",
+				AddressLastName : "",
+				AddressCompany : "",
+				AddressPhone : "",
+				AddressLine1 : "",
+				AddressLine2 : "",
+				AddressCity : "",
+				AddressProvinceCode : "",
+				AddressCountry : "",
+				AddressZip : "",
 			}
-			writeCSVRecord(writer, []string{newAddressLine.Email,newAddressLine.Item})
+			writeCSVRecord(writer, []string{
+				//basic cols - email, command, first, last, phone, note, tags, tags command
+				customer.Email, "MERGE", customer.FirstName, customer.LastName, customer.Phone,customer.Note,customer.Tags, "MERGE",
+				//address cols - command, first, last, company, phone, line1, line2, city, provinceCode, country, zip
+				"MERGE", addressVals.AddressFirstName, addressVals.AddressLastName, addressVals.AddressCompany, addressVals.AddressPhone,
+				addressVals.AddressLine1, addressVals.AddressLine2, addressVals.AddressCity, addressVals.AddressProvinceCode, 
+				addressVals.AddressCountry, addressVals.AddressZip,
+			})
 		}
 	}
 
